e6606f192a80e966ec7242d03b30da3bbcadeff4
VannessaMasterman <88731038+VannessaMasterman@users.noreply.github.com>
2021-12-07 14:51:52 -0600

    Just some cleanup

--- src/LineButton.java
+++ src/LineButton.java
@@ -1,5 +1,4 @@
 import java.awt.Cursor;
-import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
--- src/UIContext.java
+++ src/UIContext.java
@@ -1,6 +1,10 @@
-import java.awt.*;
+import java.awt.Point;
+
 public interface UIContext {
   //  public abstract void drawCircle(Circle circle);
+
   public abstract void drawLine(Point point1, Point point2 );
+
   public abstract void drawLabel(Label label);
+
 }
